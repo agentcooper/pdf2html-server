0ad5f1e709f982c50255d961e9581178bce880ce
Artem Tyurin <artem.tyurin@gmail.com>
2016-08-04 19:05:44 +0200

    Funky CSS

--- src/ViewerHighlight.js
+++ src/ViewerHighlight.js
@@ -7,10 +7,9 @@ class ViewerHighlight extends PureComponent {
     const { left, top, width, height } = position;
 
     return (
-      <div style={
+      <div className="highlight-part" style={
         {
           left, top, width, height,
-          backgroundColor: `rgba(255, 0, 0, 0.5)`,
           position: 'absolute',
           zIndex: 1,
           WebkitUserSelect: 'none',
